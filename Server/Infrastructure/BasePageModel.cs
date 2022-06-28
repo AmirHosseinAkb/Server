b9438ef2836aeca3198f6be7838ebfd996cb19c6
AmirHosseinAkb <amirbvk1@gmail.com>
2022-06-28 23:07:43 +0430

    ChangeFilesLocationsInShared

---
+++ Server/Infrastructure/BasePageModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Infrastructure
+{
+    public abstract class BasePageModel:PageModel
+    {
+        public BasePageModel():base()
+        {
+
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
+var webApplicationOptions = new WebApplicationOptions()
+{
+    EnvironmentName = Microsoft.Extensions.Hosting.Environments.Development
+};
+
+var builder = WebApplication.CreateBuilder(webApplicationOptions);
+
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Errors/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
 
 app.Run();
